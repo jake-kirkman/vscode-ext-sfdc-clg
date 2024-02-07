1b84df95d2db45b59214871c1bb0c80940fc1e5f
jake-kirkman <checko9@hotmail.co.uk>
2024-02-07 12:10:43 +0000

    Helpful comment

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,1 +1,7 @@
+/*
+ * This declaration is due to the XML formatter package having some wonky types
+ * as the TS import is simply `import xmlFormat from 'xml-formatter'`, however
+ * when this converts to JS it modified the `xmlFormat` function to `xmlFormat.default`
+ * that doesn't exist. So declaring the module here means TS will just leave it as a function
+ */
 declare module 'xml-formatter'; 
